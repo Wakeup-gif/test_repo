4637e3b14d1ff0f7a56492f85151ff316d14e4df
Wakeup-gif <cristianeliocastro@gmail.com>
2026-08-06 14:10:02 -0400

    Add canonical Scope of Work File Tools installer

---
+++ tampermonkey/US-Sign-Scope-of-Work-File-Tools.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         US Sign Scope of Work File Tools
+// @namespace    us-sign-full-modules
+// @version      2.5.1
+// @description  Adds project-folder and reference-link controls from Important Notes.
+// @match        https://ussignandmill.squarecoil.net/project.php*
+// @run-at       document-idle
+// @grant        GM_addStyle
+// @grant        GM_setClipboard
+// @grant        GM_openInTab
+// @noframes
+// @require      https://raw.githubusercontent.com/Wakeup-gif/test_repo/main/tampermonkey/US-Sign-Scope-File-Tools.user.js
+// @updateURL    https://raw.githubusercontent.com/Wakeup-gif/test_repo/main/tampermonkey/US-Sign-Scope-of-Work-File-Tools.user.js
+// @downloadURL  https://raw.githubusercontent.com/Wakeup-gif/test_repo/main/tampermonkey/US-Sign-Scope-of-Work-File-Tools.user.js
+// ==/UserScript==
